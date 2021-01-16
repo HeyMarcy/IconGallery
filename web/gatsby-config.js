29d56365cab5cef053b476f7e6f2fe16facccaec
Marcy Montross <mmontros@nerdery.com>
2021-01-16 16:08:35 -0600

    adds gatsby config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,1 +1,25 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: 'Icon Gallery',
+    siteURL: 'icongallery.com',
+    description: 'Specializing in limited edition fine art music photography.',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '1q2c3ohm',
+        dataset: 'develop',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
